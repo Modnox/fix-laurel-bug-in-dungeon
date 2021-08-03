84cc5a4238cdb42f65b5ed39227bf3596365cabc
false <marco.feder@gmx.de>
2021-08-03 17:25:36 +0200

    - updated packet version

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports = function fixLaurelBug(mod) {
-    mod.hook('S_LOAD_ACHIEVEMENT_LIST', 2, e => {
+    mod.hook('S_LOAD_ACHIEVEMENT_LIST', 3, e => {
         for (let item of e.history) {
             if (item.season < 9) {
                 item.seasonStartTime = 0n
